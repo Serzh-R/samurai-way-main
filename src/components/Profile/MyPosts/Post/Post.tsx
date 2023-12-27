ca078089553e4326ca640fc381015d170b6fed4f
Serzh-R <daarksen2@gmail.com>
2023-12-27 23:30:12 +0500

    added module.css

---
+++ src/components/Profile/MyPosts/Post/Post.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import s from './Post.module.css';
+
+
+type PostPropsType = {
+    message: string
+}
+export const Post = (props: PostPropsType) => {
+    return (
+        <div className={s.item}>
+            <img
+                src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIbVXKD4jehXtmLvVrvUlJf0oaW9gPk0YItbundagrBfveeCmKQNqVzzbYAZHcH0q4Pkw&usqp=CAU'/>
+            {props.message}
+            <div>
+                <span>like</span>
+            </div>
+
+        </div>
+    );
+};
+
